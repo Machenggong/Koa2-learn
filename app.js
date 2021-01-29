2fb816583511c37e5070125b7781b2a308a46e84
1137453716@qq.com <1137453716@qq.com>
2021-01-29 18:01:02 +0800

    fetch

--- app.js
+++ app.js
@@ -9,18 +9,32 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 const pv = require('./middleware/koa-pv')
+var cors = require("koa-cors")
+// const static = require("koa-static")
 
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
+console.log(__dirname, 'xx')
 app.use(require('koa-static')(__dirname + '/public'))
 // app.use(pv())
+
+app.use(cors({
+  origin: function (ctx) { //设置允许来自指定域名请求
+    console.log(ctx,'xx')
+    // if (ctx.url === '/') {
+    //   return '*'; // 允许来自所有域名请求
+    // }
+    return '*'; //只允许http://localhost:8080这个域名的请求
+    // return 'http://192.168.3.21:8080'; //只允许http://localhost:8080这个域名的请求
+  }
+}));
 
 
 app.use(views(__dirname + '/views', {
@@ -39,8 +53,8 @@ app.use(async (ctx, next) => {
 // allowedMethods 有两个作用。
 // 1：在跨域请求时，首先会触发option请求，如果没有allowedMethods，则不会触发option，
 // 2.可以返回请求头，多出一个参数allow,提示请求方法错误
-app.use(index.routes()).use(index.allowedMethods())
-app.use(users.routes()).use(users.allowedMethods())
+app.use(index.routes()).use(index.allowedMethods());
+app.use(users.routes()).use(users.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const router = require('koa-router')()
 var fs = require('fs')
-const parseXlsx= require('excel') ;
-const Xlsx= require('node-xlsx') ;
-const { nextTick } = require('process');
- 
+const parseXlsx = require('excel');
+const Xlsx = require('node-xlsx');
+const {
+  nextTick
+} = require('process');
+const {
+  request
+} = require('http');
+
 
 
 
@@ -17,30 +22,74 @@ router.get('/', async (ctx, next) => {
 
 })
 
-router.get('/string', async (ctx,next)=>{
+// 如果是get请求： 获取用户输入的参数：ctx.request.query,  如果是post请求，获取用户的输入参数：ctx.request.body
+router.get('/string', async (ctx, next) => {
   ctx.user = "text";
+  // let src = [1,2,3,4,5]
+  // let x = src.filter(item=> item !== 3)
+  // let y = src.reduce((pre,item,index)=>{
+  //   console.log(pre,'presssssssssssssss')
+  //   return pre += item
+  // },0)
+  // console.log(x)
+  // console.log(y)
+  let code = "http%3A%2F%2Fwww.w3school.com.cn%2FMy%20first%2F";
+  console.log(decodeURIComponent(code))
   next();
-},async (ctx, next) => {
-  console.log(ctx)
-  console.log(ctx.user,'ussxxxxxxxxxxxxxxx')
+}, async (ctx, next) => {
+  // console.log(ctx.request.query,'aaaaaaaaaaaa')
+  // console.log(ctx.request.querystring,'bbbbbbbbbbbb')
+  // console.log(ctx.user, 'ussxxxxxxxxxxxxxxx')
+  // console.log(ctx.req,'ddddddddddddddd')
   ctx.body = {
-    status:200,
-    text:'koa2 string'
+    status: 200,
+    query: ctx.request.query,
+    querystring: ctx.request.querystring,
+    ctx_query: ctx.query,
+    ctx_querystring: ctx.querystring
   }
 })
 
+
+router.post('/postdata', async (ctx, next) => {
+  global.console.log(ctx.request.body,"xaa")
+  let { id , name } = JSON.parse(ctx.request.body)
+  ctx.body = {
+    status:200,
+    msg:"post请求成功",
+    data:{
+      id: id,
+      name:name
+    }
+  }
+})
 router.get('/json', async (ctx, next) => {
   var ad = "json Data"
-  var ad1 = JSON.stringify(ad);
   console.log(typeof ad)
-  console.log(ad1)
+  console.log(ctx.cookies.get("userName"),'name')
   ctx.body = {
     title: 'koa2 json'
   }
 })
+router.get('/setcookie', async (ctx, next) => {
+  ctx.cookies.set("userName", 'mcg_serve', {
+    domain: "",
+    // path: "/setcookie",
+    maxAge: 1000 * 20,
+    expires: new Date('2021-1-28'),
+    httpOnly: false,
+    overwrite: false
+  })
+  next()
+}, async (ctx, next) => {
+  console.log(ctx.cookies.get("userName"), '1')
+  ctx.body = {
+    title: 'Cookie写入成功'
+  }
+})
 
 router.get('/areas', async (ctx, next) => {
-  let jsonData={}
+  let jsonData = {}
   await new Promise((resolve, reject) => {
     fs.readFile('public/untils/area.js', (err, data) => {
       if (err) {
@@ -52,8 +101,8 @@ router.get('/areas', async (ctx, next) => {
     })
   })
   const buffer = new Buffer(jsonData, 'utf-8');
-  ctx.body={
-    title:buffer
+  ctx.body = {
+    title: buffer
   }
 })
 
